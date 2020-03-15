d726edf77bfafe84b1c83dc3cdee3fc19f9baf06
fearclear <fearcleari@gmail.com>
2020-03-15 21:31:54 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
+import genTable from './utils'
+
 // @ts-ignore
 Component({
   properties: {
-    prop: {
-      type: String,
-      value: 'index.properties'
+    scroll: {
+      type: Object,
+      value: {
+        x: undefined,
+        y: undefined
+      }
     },
+    dataSource: {
+      type: Array,
+      value: [],
+      observer() {
+        this.genTable()
+      }
+    },
+    columns: {
+      type: Array,
+      value: [],
+      observer() {
+        this.genTable()
+      }
+    }
   },
   data: {
     flag: false,
+    nodes: []
   },
   lifetimes: {
     attached() {
-      console.log('attched') // eslint-disable-line
+      const options = {
+        scroll: this.properties.scroll,
+        columns: this.properties.columns,
+        dataSource: this.properties.dataSource
+      }
+      const table = genTable(options)
+      this.setData({
+        nodes: [table]
+      })
+    }
+  },
+  methods: {
+    genTable() {
+      const options = {
+        scroll: this.properties.scroll,
+        columns: this.properties.columns,
+        dataSource: this.properties.dataSource
+      }
+      const table = genTable(options)
+      this.setData({
+        nodes: [table]
+      })
     }
   }
 })
